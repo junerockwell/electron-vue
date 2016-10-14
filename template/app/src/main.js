24ed71cd224d315365220db48542c58b5c4930e2
June Rockwell <cherubeflugel@gmail.com>
2016-10-14 10:47:09 -0700

    Update main.js

--- template/app/src/main.js
+++ template/app/src/main.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
-{{#isEnabled plugins 'vue-electron'}}
-import Electron from 'vue-electron'
-{{/isEnabled}}
-{{#isEnabled plugins 'vue-resource'}}
-import Resource from 'vue-resource'
-{{/isEnabled}}
-{{#isEnabled plugins 'vue-router'}}
-
 import App from './App'
-import router from './router'
-{{/isEnabled}}
 
-{{#isEnabled plugins 'vue-electron'}}
+import Electron from 'vue-electron'
+import Resource from 'vue-resource'
+
+import router from './router'
+
+
+
 Vue.use(Electron)
 {{/isEnabled}}
 {{#isEnabled plugins 'vue-resource'}}
@@ -19,28 +15,12 @@ Vue.use(Resource)
 {{/isEnabled}}
 Vue.config.debug = true
 
-{{#isEnabled plugins 'vue-router'}}
 /* eslint-disable no-new */
+//{{#if_eq vueVersion '2.x'}}
 new Vue({
   router,
   ...App
 }).$mount('#app')
-{{else}}
-router.start(App, 'app')
-{{/if_eq}}
-{{else}}
-import App from './App'
 
-/* eslint-disable no-new */
-{{#if_eq vueVersion '2.x'}}
-new Vue({
-  router,
-  ...App
-}).$mount('#app')
-{{else}}
-new Vue({
-  el: 'body',
-  components: { App }
-})
-{{/if_eq}}
-{{/isEnabled}}
+//{{/if_eq}}
+
